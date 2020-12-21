1c68c92f86eb3e59200eae19b6d030a4cda95840
littlemelonling <sillosougri270415@gmail.com>
2020-12-21 03:50:44 -0800

    remove pygraphviz

--- fsm.py
+++ fsm.py
@@ -1,11 +1,12 @@
-from transitions.extensions import GraphMachine
+#from transitions.extensions import GraphMachine
 
 from utils import send_text_message
 
 
-class TocMachine(GraphMachine):
+class TocMachine("""GraphMachine"""):
     def __init__(self, **machine_configs):
-        self.machine = GraphMachine(model=self, **machine_configs)
+        #self.machine = GraphMachine(model=self, **machine_configs)
+        a = 0
 
     def is_going_to_state1(self, event):
         text = event.message.text
