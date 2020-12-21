01f2571241c26983d5f9556792817f08f3b14947
littlemelonling <sillosougri270415@gmail.com>
2020-12-21 03:54:28 -0800

    remove pygraphviz

--- fsm.py
+++ fsm.py
@@ -3,7 +3,8 @@
 from utils import send_text_message
 
 
-class TocMachine("""GraphMachine"""):
+#class TocMachine(GraphMachine):
+class TocMachine():
     def __init__(self, **machine_configs):
         #self.machine = GraphMachine(model=self, **machine_configs)
         a = 0
